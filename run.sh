571009b181e00a760229eb2b9b86550a275b81be
Josh Beam <josh@joshbeam.com>
2012-03-29 11:03:24 -0500

    Made DromeGui and DromeDemo not be built by CMake for the time being.

--- run.sh
+++ run.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 cd `dirname $0`
-
-# remove/create the build directory if necessary
-if [ "$1" = "clean" ]; then
-	rm -rf build 2> /dev/null
-fi
-mkdir build 2> /dev/null
-
-# build
-cd build
-cmake .. && make
+./build.sh
 
 # execute DromeDemo if the build was successful
 if [ $? -eq 0 ]; then
