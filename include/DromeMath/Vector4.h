6b015c82ec7b7dd0b651a216bff9d146d6053927
Josh Beam <josh@joshbeam.com>
2012-04-09 21:39:02 -0500

    Added the Vector4 class.

---
+++ include/DromeMath/Vector4.h
@@ -0,0 +1,204 @@
+/*
+ * Copyright (C) 2010-2012 Josh A. Beam
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __DROMEMATH_VECTOR4_H__
+#define __DROMEMATH_VECTOR4_H__
+
+#include <string>
+
+namespace DromeMath {
+
+/**
+ * \brief Represents a four-dimensional vector.
+ */
+class Vector4
+{
+	public:
+		/**
+		 * \brief The X component of the vector.
+		 */
+		float x;
+
+		/**
+		 * \brief The Y component of the vector.
+		 */
+		float y;
+
+		/**
+		 * \brief The Z component of the vector.
+		 */
+		float z;
+
+		/**
+		 * \brief The W component of the vector.
+		 */
+		float w;
+		
+		/**
+		 * Creates a new Vector4 object representing the vector with the given coordinates.
+		 *
+		 * @param x_param The X coordinate of the vector.
+		 * @param y_param The Y coordinate of the vector.
+		 * @param z_param The Z coordinate of the vector.
+		 * @param w_param The W coordinate of the vector.
+		 */
+		Vector4(float x_param = 0.0f, float y_param = 0.0f, float z_param = 0.0f, float w_param = 0.0f);
+
+		/**
+		 * Creates a new Vector4 object from a string representation of a vector.
+		 *
+		 * @param s The string representation of the vector.
+		 */
+		Vector4(const char *s);
+
+		/**
+		 * Creates a new Vector4 object from a string representation of a vector.
+		 *
+		 * @param s The string representation of the vector.
+		 */
+		Vector4(const std::string &s);
+
+		/**
+		 * Calculates the dot product of this vector with another vector, which is defined as the sum of each component of this vector multiplied by the corresponding component in the other vector.
+		 *
+		 * @param v The vector to take the dot product of with this vector.
+		 * @return The dot product of this vector and the given vector.
+		 */
+		float dotProduct(const Vector4 &v) const;
+
+		/**
+		 * Calculates the length (or magnitude) of this vector, which is defined as the square root of the sum of the squared components of the vector (sqrt(x^2 + y^2 + z^2)).
+		 *
+		 * @return The length (or magnitude) of this vector.
+		 */
+		float length() const;
+
+		/**
+		 * Calculates the normalized version of this vector, which is a new vector that has the same direction as this vector but a length (magnitude) of 1.
+		 *
+		 * @return A Vector4 object that is the result of normalizing this vector.
+		 */
+		Vector4 normalize() const;
+
+		/**
+		 * @param v The vector to add to this vector.
+		 * @return A Vector4 object that is the result of adding each component of the given vector to the corresponding components of this vector.
+		 */
+		Vector4 operator + (const Vector4 &v) const;
+
+		/**
+		 * @param f The scalar to add to this vector.
+		 * @return A Vector4 object that is the result of adding the given scalar to each component of this vector.
+		 */
+		Vector4 operator + (float f) const;
+
+		/**
+		 * Adds each component of the given vector to the corresponding components of this vector.
+		 *
+		 * @param v The vector to add to this vector.
+		 */
+		void operator += (const Vector4 &v);
+
+		/**
+		 * Adds the given scalar to each component of this vector.
+		 *
+		 * @param f The scalar to add to this vector.
+		 */
+		void operator += (float f);
+
+		/**
+		 * @param v The vector to subtract from this vector.
+		 * @return A Vector4 object that is the result of subtracting each component of the given vector from the corresponding components of this vector.
+		 */
+		Vector4 operator - (const Vector4 &v) const;
+
+		/**
+		 * @param f The scalar to subtract from this vector.
+		 * @return A Vector4 object that is the result of subtracting the given scalar from each component of this vector.
+		 */
+		Vector4 operator - (float f) const;
+
+		/**
+		 * Subtracts each component of the given vector from the corresponding components of this vector.
+		 *
+		 * @param v The vector to subtract from this vector.
+		 */
+		void operator -= (const Vector4 &v);
+
+		/**
+		 * Subtracts the given scalar from each component of this vector.
+		 *
+		 * @param f The scalar to subtract from this vector.
+		 */
+		void operator -= (float f);
+
+		/**
+		 * @param v The vector to multiply this vector by.
+		 * @return A Vector4 object that is the result of multiplying each component of this vector by the corresponding components of the given vector.
+		 */
+		Vector4 operator * (const Vector4 &v) const;
+
+		/**
+		 * @param f The scalar to multiply this vector by.
+		 * @return A Vector4 object that is the result of multiplying each component of this vector by the given scalar.
+		 */
+		Vector4 operator * (float f) const;
+
+		/**
+		 * Multiplies each component of this vector by the corresponding components of the given vector.
+		 *
+		 * @param v The vector to multiply this vector by.
+		 */
+		void operator *= (const Vector4 &v);
+
+		/**
+		 * Multiplies each component of this vector by the given scalar.
+		 *
+		 * @param f The scalar to multiply this vector by.
+		 */
+		void operator *= (float f);
+
+		/**
+		 * @param f The scalar to divide this vector by.
+		 * @return A Vector4 object that is the result of dividing each component of this vector by the given scalar.
+		 */
+		Vector4 operator / (float f) const;
+
+		/**
+		 * Divides each component of this vector by the given scalar.
+		 *
+		 * @param f The scalar to divide this vector by.
+		 */
+		void operator /= (float f);
+
+		/**
+		 * @return A string representation of the vector.
+		 */
+		std::string toString() const;
+};
+
+} // namespace DromeMath
+
+#endif /* __DROMEMATH_VECTOR4_H__ */
---
+++ src/DromeMath/Vector4.cpp
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2010-2012 Josh A. Beam
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <DromeMath/Vector4.h>
+#include <DromeMath/Util.h>
+#include <sstream>
+
+using namespace std;
+
+namespace DromeMath {
+
+Vector4::Vector4(float x_param, float y_param, float z_param, float w_param)
+{
+	x = x_param;
+	y = y_param;
+	z = z_param;
+	w = w_param;
+}
+
+Vector4::Vector4(const char *s)
+{
+	stringstream stream(s);
+	stream >> x >> y >> z >> w;
+}
+
+Vector4::Vector4(const string &s)
+{
+	stringstream stream(s);
+	stream >> x >> y >> z >> w;
+}
+
+float
+Vector4::dotProduct(const Vector4 &v) const
+{
+	return x * v.x + y * v.y + z * v.z + w * v.w;
+}
+
+float
+Vector4::length() const
+{
+	return sqrtf(dotProduct(*this));
+}
+
+Vector4
+Vector4::normalize() const
+{
+	float scale = 1.0f / sqrtf(x*x + y*y + z*z + w*w);
+	return Vector4(x * scale, y * scale, z * scale, w * scale);
+}
+
+Vector4
+Vector4::operator + (const Vector4 &v) const
+{
+	return Vector4(x + v.x, y + v.y, z + v.z, w + v.w);
+}
+
+Vector4
+Vector4::operator + (float f) const
+{
+	return Vector4(x + f, y + f, z + f, w + f);
+}
+
+void
+Vector4::operator += (const Vector4 &v)
+{
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	w += v.w;
+}
+
+void
+Vector4::operator += (float f)
+{
+	x += f;
+	y += f;
+	z += f;
+	w += f;
+}
+
+Vector4
+Vector4::operator - (const Vector4 &v) const
+{
+	return Vector4(x - v.x, y - v.y, z - v.z, w - v.w);
+}
+
+Vector4
+Vector4::operator - (float f) const
+{
+	return Vector4(x - f, y - f, z - f, w - f);
+}
+
+void
+Vector4::operator -= (const Vector4 &v)
+{
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+	w -= v.w;
+}
+
+void
+Vector4::operator -= (float f)
+{
+	x -= f;
+	y -= f;
+	z -= f;
+	w -= f;
+}
+
+Vector4
+Vector4::operator * (const Vector4 &v) const
+{
+	return Vector4(x * v.x, y * v.y, z * v.z, w * v.w);
+}
+
+Vector4
+Vector4::operator * (float f) const
+{
+	return Vector4(x * f, y * f, z * f, w * f);
+}
+
+void
+Vector4::operator *= (const Vector4 &v)
+{
+	x *= v.x;
+	y *= v.y;
+	z *= v.z;
+	w *= v.w;
+}
+
+void
+Vector4::operator *= (float f)
+{
+	x *= f;
+	y *= f;
+	z *= f;
+	w *= f;
+}
+
+Vector4
+Vector4::operator / (float f) const
+{
+	return Vector4(x / f, y / f, z / f, w / f);
+}
+
+void
+Vector4::operator /= (float f)
+{
+	x /= f;
+	y /= f;
+	z /= f;
+	w /= f;
+}
+
+string
+Vector4::toString() const
+{
+	stringstream stream;
+	stream << x << " " << y << " " << z << " " << w;
+
+	return stream.str();
+}
+
+} // namespace DromeMath
