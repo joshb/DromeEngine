64ec2e0de0865d377e66b237433b89f3015db21a
Josh Beam <josh@joshbeam.com>
2012-09-15 17:49:56 -0500

    Modified the GLWindow_GLFW class to enable texturing and blending.

--- src/DromeGL/GLWindow_GLFW.cpp
+++ src/DromeGL/GLWindow_GLFW.cpp
@@ -503,6 +503,9 @@ GLWindow_GLFW::open()
 	glClearColor(0.25f, 0.25f, 0.5f, 1.0f);
 	glClearDepth(1.0f);
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	glEnable(GL_CULL_FACE);
 	glFrontFace(GL_CCW);
