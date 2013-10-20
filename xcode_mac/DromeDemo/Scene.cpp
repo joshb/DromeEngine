b41182e3dd5a00f3a443f57d6b0cf9ecb31b8c96
Josh Beam <josh@joshbeam.com>
2013-10-19 19:35:04 -0500

    Added an EventHandler.h include to the DromeCore header.

---
+++ xcode_mac/DromeDemo/Scene.cpp
@@ -0,0 +1,9 @@
+//
+//  Scene.cpp
+//  DromeDemo
+//
+//  Created by Josh Beam on 10/19/13.
+//
+//
+
+#include "Scene.h"
---
+++ xcode_mac/DromeDemo/Scene.h
@@ -0,0 +1,14 @@
+//
+//  Scene.h
+//  DromeDemo
+//
+//  Created by Josh Beam on 10/19/13.
+//
+//
+
+#ifndef __DromeDemo__Scene__
+#define __DromeDemo__Scene__
+
+#include <iostream>
+
+#endif /* defined(__DromeDemo__Scene__) */
